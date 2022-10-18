5c5a3b8026e6171f9a813cf41e126cbca8320c08
dta90d <dta90d@keemail.me>
2022-10-18 18:15:42 +0300

    Init.

---
+++ src/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+button,
+input,
+optgroup,
+select,
+textarea {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+}
+
+html {
+    font-size: 12px;
+}
+
+body {
+    @apply font-roboto text-text-main;
+    font-size: 1.4rem;
+    overflow-x: hidden;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: transparent;
+}
+
+</style>
